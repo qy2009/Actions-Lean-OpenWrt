97831381147292c57b5255ad4e7e0b18164ea866
qy2009 <51539377+qy2009@users.noreply.github.com>
2020-03-23 16:47:06 -0700

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,7 @@
 #=================================================
 # Modify default IP
 sed -i 's/10.0.0.1/10.0.0.254/g' package/base-files/files/bin/config_generate
+# 添加第三方软件包
+#git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+git clone https://github.com/jefferymvp/luci-app-koolproxyR package/luci-app-koolproxyR
