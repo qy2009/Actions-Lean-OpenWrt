48ed46e2c3722930d2649dcf6c67034ea68c3c25
qy2009 <51539377+qy2009@users.noreply.github.com>
2020-03-24 10:14:51 -0700

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -11,3 +11,5 @@ sed -i 's/10.0.0.1/10.0.0.254/g' package/base-files/files/bin/config_generate
 #git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 git clone https://github.com/jefferymvp/luci-app-koolproxyR package/luci-app-koolproxyR
+cd openwrt && ./scripts/feeds update -a
+cd openwrt && ./scripts/feeds install -a
